7ac8cdb498c5c54181a52f0c22f193671dca8da6
Onishi <24ci0204@jec.ac.jp>
2025-06-25 14:27:10 +0900

    プレイヤーに動きをつけつつある

---
+++ PartyGame/Assets/Script/Onishi/Onishi_TestPlayer.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Onishi_TestPlayer : PlayerParent
+{
+    protected override void Start()
+    {
+        base.Start();
+    }
+
+    protected override void MoveUpdata(Vector2 vec)
+    {
+        Vector3 pos = this.transform.position;
+        pos += new Vector3(vec.x, 0, vec.y);
+        transform.position = pos;
+        Debug.Log(vec);
+    }
+
+    protected override void LookUpdata(Vector2 vec)
+    {
+        
+    }
+
+    protected override void OnButtonA() {
+
+        Debug.Log("user"+ playerData.GetUserValue() +"OnButtonA");
+    }
+
+    protected override void UpButtonA() { }
+
+    protected override void OnButtonB() { }
+
+    protected override void UpButtonB() { }
+
+    protected override void OnButtonX() { }
+
+    protected override void UpButtonX() { }
+
+    protected override void OnButtonY() { }
+
+    protected override void UpButtonY() { }
+}
---
+++ PartyGame/Assets/Script/Onishi/Onishi_TestSceneManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class Onishi_TestSceneManager : SceneManagerParent
+{
+    protected override Type PlayerType()
+    {
+        return typeof(Onishi_TestPlayer);
+    }
+
+    protected override void UnityUpdate()
+    {
+        
+    }
+
+    protected override string PlayerFilePath(int index)
+    {
+        string str =
+            "Player/Test/Cube_" + index.ToString();
+
+        return str;
+    }
+}
