88fdd681df565a38decca5ed3dea1d3bf26b1932
1noshin <bisu8979@icloud.com>
2025-07-02 15:57:48 +0900

    titleSceneの編集

--- PartyGame/Assets/Script/Title/TitleController.cs
+++ PartyGame/Assets/Script/Title/TitleController.cs
@@ -4,13 +4,9 @@ using UnityEngine.SceneManagement;
 
 public class TitleController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
+
+
     void Update()
     {
         if(Input.anyKeyDown)
