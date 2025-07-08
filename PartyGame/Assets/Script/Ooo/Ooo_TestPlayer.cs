5c2288045eb47aec5ea5f7c9d19b6c02bafd0737
sungmin <24ci0203@jec.ac.jp>
2025-07-08 11:35:36 +0900

    waterbomb Prefab / script 作成 player script update

--- PartyGame/Assets/Script/Ooo/Ooo_TestPlayer.cs
+++ PartyGame/Assets/Script/Ooo/Ooo_TestPlayer.cs
@@ -1,10 +1,25 @@
 ﻿using UnityEngine;
 using UnityEngine.InputSystem;
+using System.Collections;
 
 public class Ooo_TestPlayer : PlayerParent
 {
     Vector3 moveVec;
+
+    //プレイヤー関連設定
     float plSpeed = 10.0f;
+    public GameObject waterbombPrefab;
+
+    //Waterbomb関連設定
+    public float waterbombTime = 3f;    //基本爆発時間は3秒(囲まれたら3秒）
+    public float escapeTimeUp = 0.3f;   //連打すれば0.3秒ずつ減少
+    public int maxEscapeClick = 10;     //最大連打可能回数（3秒内に10回押したら脱出可能）
+
+    private bool isTrapped = false;    //相手のWaterbombに囲まれたか
+    private float nowEscapeClick = 0;   //現在脱出ボタンを押した回数
+    
+
+   
 
 
     protected override void Start()
@@ -14,12 +29,18 @@ public class Ooo_TestPlayer : PlayerParent
 
     private void Update()
     {
-        transform.position += moveVec * plSpeed * Time.deltaTime;
+        if (!isTrapped)    //囲まれてなかったら普通に移動できる
+        {
+            transform.position += moveVec * plSpeed * Time.deltaTime;
+        }
     }
 
     protected override void MoveUpdate(Vector2 vec)
     {
-        moveVec = new Vector3(vec.x, 0, vec.y);
+        if (!isTrapped)    //囲まれてなかったら移動できる
+        {
+            moveVec = new Vector3(vec.x, 0, vec.y);
+        }
     }
 
     protected override void LookUpdate(Vector2 vec)
@@ -29,15 +50,28 @@ public class Ooo_TestPlayer : PlayerParent
 
     protected override void OnButtonA()
     {
-
         Debug.Log("user" + playerData.GetUserValue() + "OnButtonA");
+
+
+        if(!isTrapped)     //囲まれてなかったら
+        {
+            ThrowBomb();    //Aボタン押したらWaterbombをプレイヤの現在位置に置く
+        }
     }
 
     protected override void UpButtonA() { }
 
     protected override void OnButtonB()
     {
+        if(isTrapped)  //囲まれたら
+        {
+            nowEscapeClick++;   //Bボタン押すたびに +1
 
+            if(nowEscapeClick >= maxEscapeClick)    //3秒内に10回以上押したら
+            {
+                Escape();   //水風船から脱出！
+            }
+        }
     }
 
     protected override void UpButtonB() { }
@@ -49,4 +83,28 @@ public class Ooo_TestPlayer : PlayerParent
     protected override void OnButtonY() { }
 
     protected override void UpButtonY() { }
+
+
+    void ThrowBomb()    //Waterbombを置く関数
+    {
+        if(waterbombPrefab != null)
+        {
+            //今のプレイヤの位置に水風船配置
+            GameObject waterbomb = Instantiate(waterbombPrefab, transform.position, Quaternion.identity);
+
+            //誰がwaterbombを配置したのか
+            Ooo_Waterbomb ooo_waterbomb = waterbomb.GetComponent<Ooo_Waterbomb>();  //Waterbombスクリプト
+            if(ooo_waterbomb != null)
+            {
+                ooo_waterbomb.Initialize(playerData.GetUserValue());
+            }
+            
+        }
+    }
+
+    void Escape()
+    {
+        isTrapped = false;  //水風船から脱出
+        nowEscapeClick = 0; //Bボタン押した回数初期化
+    }
 }
---
+++ PartyGame/Assets/Script/Ooo/Ooo_Waterbomb.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ooo_Waterbomb : MonoBehaviour
+{
+    //プレイヤーが水風船を置くと、点滅して爆発した範囲内の相手プレイヤー囲む
+
+    public float explodeTime = 3f;      //3秒経ったら爆発
+    public float explodeRadius = 3f;    //爆発範囲
+
+    public GameObject expoldeEffect;    //爆発エフェクトPrefab
+
+    private int waterBombPlayerId;      //誰がwaterbombを置いたのか保存
+
+    void Start()
+    {
+        
+    }
+
+    public void Initialize(int playerId)    //waterbombがおかれれば
+    {
+        waterBombPlayerId = playerId;       //誰が置いたのか保存
+        StartCoroutine(ExplodeRoutine());   //爆発課程スタート
+    }
+
+    void Update()
+    {
+        
+    }
+
+    IEnumerator ExplodeRoutine()
+    {
+        float blinkTime = 1.5f;     //1.5秒間点滅
+        yield return new WaitForSeconds(blinkTime);
+
+        //置いたら1.5秒後点滅→1.5秒間点滅後爆発
+        Explode();
+    }
+
+    void Explode()
+    {
+
+    }
+}
