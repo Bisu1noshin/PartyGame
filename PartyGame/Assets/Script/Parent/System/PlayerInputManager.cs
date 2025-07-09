aed5a86fbcf957cdc440bd670d13e047e57b6274
1noshin <bisu8979@icloud.com>
2025-07-09 16:41:14 +0900

    s\InputSystem

---
+++ PartyGame/Assets/Script/Parent/System/PlayerInputManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Users;
+
+public class PlayerInputManager : MonoBehaviour
+{
+    private PartyGame inputAction;
+
+    private void Awake()
+    {
+        inputAction = new PartyGame();
+
+        
+        //InputUser.PerformPairingWithDevice()
+    }
+}
