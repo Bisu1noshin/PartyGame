2c5103295eb312f118cf369c648652cce274595b
1noshin <bisu8979@icloud.com>
2025-07-15 19:23:20 +0900

    no message

--- PartyGame/Assets/Script/Parent/SceneManager/InGameManeger.cs
+++ PartyGame/Assets/Script/Parent/SceneManager/InGameManeger.cs
@@ -108,7 +108,7 @@ public abstract class InGameManeger : MonoBehaviour
 
                 for (int i = 0; i < maxPlayerCount; i++) {
 
-                    Vector3 vector3 = Vector3.zero;
+                    Vector3 vector3 =  Vector3.zero;
                     Quaternion quat = Quaternion.identity;
                     GameObject prefab =
                         Resources.Load<GameObject>(SetPlayerPrefab(0));
