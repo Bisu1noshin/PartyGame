2001e4f081d1aee12e4b33c1ac095ceddeb85acb
24CI0210 <mizukikamedajp@gmail.com>
2025-07-23 19:28:32 +0900

    FBXデータと回転制御を作成

---
+++ PartyGame/Assets/Data/FBX/girl_yellow/Girl_yellow_RotationFixer.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class Girl_yellow_RotationFixer : RotationFixer
+{
+    protected override void IdleRotate()
+    {
+        transform.rotation = Quaternion.Euler(-2, 0, 4);
+    }
+    protected override void RunningRotate()
+    {
+        transform.rotation = Quaternion.Euler(11, 0, -2);
+    }
+}
