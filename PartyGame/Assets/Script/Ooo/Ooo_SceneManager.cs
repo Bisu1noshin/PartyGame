22029ca811148a8aafdb647742298e419ba9d3af
Bisu1noshin <24ci0211@jec.ac.jp>
2025-07-30 13:36:13 +0900

    naosita

--- PartyGame/Assets/Script/Ooo/Ooo_SceneManager.cs
+++ PartyGame/Assets/Script/Ooo/Ooo_SceneManager.cs
@@ -162,24 +162,7 @@ public class Ooo_SceneManager : InGameManeger
 
     public override string SceneName => "TitleScene";
 
-    public override void OnLoaded(PlayerInformation[] data)
-    {
-
-        if (data is null || data is not PlayerInformation[] playerInformation)
-        {
-            Debug.LogError("data is null");
-            return;
-        }
-
-        // presenterを取得して、Presenter側の初期化メソッドを実行して、シーン全体を動かす
-        var presenter = UnityEngine.Object.FindAnyObjectByType<InGameManeger>();
-        presenter.SetPlayerInformation(playerInformation);
-    }
+    
     public override void OnUnLoaded() { }
 
-    protected override void NextSceneJump()
-    {
-
-        SSceneManager.LoadScene<Ooo_SceneManager>(playerInformation).Forget();
-    }
 }
